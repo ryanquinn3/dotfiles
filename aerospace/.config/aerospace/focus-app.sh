ecb0707efeb834d8df8216ec67a4bbcb1b5ba7c8
ryanquinn3 <ryan3q@gmail.com>
2025-09-05 11:16:02 -0400

    hotkey to focus on app

---
+++ aerospace/.config/aerospace/focus-app.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+APP_ID=$1
+APP_NAME=$2
+
+focus_app() {
+  current_workspace=$(aerospace list-workspaces --focused)
+  app_window_id=$(aerospace list-windows --all --format "%{window-id}%{right-padding} | %{app-name}" | grep $APP_NAME | cut -d' ' -f1 | sed '1p;d')
+  aerospace focus --window-id $app_window_id
+  aerospace move-mouse window-lazy-center
+}
+
+app_closed() {
+  if [ "$(aerospace list-windows --all --format '%{app-name}' | grep $APP_NAME)" == "" ]; then
+    true
+  else
+    false
+  fi
+}
+
+app_focused() {
+  if [ "$(aerospace list-windows --focused --format "%{app-bundle-id}")" == "$APP_ID" ]; then
+    true
+  else
+    false
+  fi
+}
+
+unfocus_app() {
+  aerospace move-node-to-workspace scratchpad
+}
+
+if app_closed; then
+  echo "App is closed"
+else
+  if app_focused; then
+    echo "App is already focus"
+  else
+    focus_app
+  fi
+fi
