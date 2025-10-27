06e784e5712fd0dd45950a2a51d8108a0b065d83
ryanquinn3 <ryan3q@gmail.com>
2025-10-27 07:43:08 -0400

    claude + stow

--- stow.sh
+++ stow.sh
@@ -6,13 +6,19 @@ function run_stow(){
   done
 }
 
-run_stow zsh docker tmux git-common fd lnav bin
+run_stow zsh  tmux git-common fd lnav bin
 
 
 if [[ $OSTYPE == 'darwin'* ]]; then
-  run_stow aerospace ghostty personal sketchybar
+  run_stow aerospace ghostty personal sketchybar docker
 fi
 
 if [[ $OSTYPE == 'linux-gnu'* ]]; then
   run_stow codespace claude
+  if [ -f ~/.docker/config.json ]; then
+    jq -s '.[0] * .[1]' ~/.docker/config.json $DOT_FILES/docker/.docker/config.json > /tmp/merged_docker_config.json
+    mv /tmp/merged_docker_config.json $HOME/.docker/config.json
+  else
+    run_stow docker
+  fi
 fi
